12f5cbe50b3bf4a894178ba6e851d6bb034cb6b4
Shirley Zhou <shirleyzhou7@gmail.com>
2018-11-27 17:58:14 -0500

    locally added bootstrap files

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+// var bootstrap = require('bootstrap');
+// require('bootstrap/dist/css/bootstrap.css');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
